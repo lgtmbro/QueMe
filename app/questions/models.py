aae91ae8881d0df1065bdb62a04592e5248b2030
Callum Fleming <howzitcdf@gmail.com>
2022-05-03 06:03:50 +0200

    basic models added

--- app/questions/models.py
+++ app/questions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Customers(models.Model):
+    msisdn = models.TextField()
+    profile_name = models.TextField()
+
+
+class Questions(models.Model):
+    content = models.TextField()
+    active = models.BooleanField(default=False)
+
+
+class Asks(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    answer = models.TextField()
+    status = models.TextField()
